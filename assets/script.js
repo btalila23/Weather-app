4922a763b866e2291dfc7cbed2f959da3542a71e
Talila Burka <btalila@yahoo.com>
2020-02-01 20:31:37 -0600

    added the classes and appended content

--- assets/script.js
+++ assets/script.js
@@ -104,4 +104,25 @@ $("#searchBtn").on("click", function() {
             let hour = results[i].dt_txt.split('-')[2].split(' ')[1];
             console.log(day);
             console.log(hour);
-      
+            if(results[i].dt_txt.indexOf("12:00:00") !== -1){
+        
+                // get the temperature and convert to fahrenheit 
+                let temp = (results[i].main.temp - 273.15) * 1.80 + 32;
+                let tempF = Math.floor(temp);
+        
+                const card = $("<div>").addClass("card col-md-2 ml-4 bg-primary text-white");
+                const cardBody = $("<div>").addClass("card-body p-3 forecastBody")
+                const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+                const temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF + " °F");
+                const humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + results[i].main.humidity + "%");
+        
+                const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results[i].weather[0].icon + ".png")
+        
+                cardBody.append(cityDate, image, temperature, humidity);
+                card.append(cardBody);
+                $("#forecast").append(card);
+        
+              }
+            }
+          });
+        }
